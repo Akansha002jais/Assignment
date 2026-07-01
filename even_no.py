7699c978db4cc5936b20b7590cd41fc2b4a055b6
Akansha002jais <akanshaj883@gmail.com>
2026-07-01 18:43:01 +0530

    My first commit

---
+++ even_no.py
@@ -0,0 +1,9 @@
+n= int(input("enter the number :"))
+arr=[]
+for i in range(n):
+    num= int(input("enter the elements :"))
+    arr.append(num)
+print("even numbers in list are:")   
+for i in arr:
+    if i%2==0:
+        print(i)
